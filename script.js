ff9d3d8ad4fde450bda6cb88f496f519b24fb6fa
TiredQuan <85377764+TiredQuan@users.noreply.github.com>
2021-08-21 10:32:12 +0700

    Update script.js

--- script.js
+++ script.js
@@ -3,19 +3,22 @@ let questionActive = document.querySelectorAll('.questions')
 console.log(answers)
 document.querySelectorAll('.questions').forEach(item => {
     item.addEventListener('click', event => {
-      console.log('clock')
-      for(var i = 0; i < answers.length; i++)
-  {
-  answers[i].classList.remove('show')
-  }
-
-      for(var i = 0; i < questionActive.length; i++)
-  {
-  questionActive[i].classList.remove('question__active')
-  }
-
-      item.classList.toggle('question__active')
-      item.nextElementSibling.classList.toggle('show')
+        if(item.classList.contains('question__active')){
+        } else {
+            
+            for(var i = 0; i < answers.length; i++)
+            {
+                answers[i].classList.remove('show')
+            }
+            for(var i = 0; i < questionActive.length; i++)
+            {
+                questionActive[i].classList.remove('question__active')
+            }
+        }
+        item.classList.toggle('question__active')
+        item.nextElementSibling.classList.toggle('show')
+        
+            
     })
     // item.addEventListener('blur', event => {
         //     console.log('bruh')
